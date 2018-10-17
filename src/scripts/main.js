c1343dc2564d4eec1c95df3606d5f9f1db79a940
Trung Pham <someonelikeyou1620@gmail.com>
2018-10-17 21:43:58 +0700

    complete

---
+++ src/scripts/main.js
@@ -0,0 +1,54 @@
+// Main
+console.log('FullStack Vietnam!')
+
+$('.home_slider .owl-carousel').owlCarousel({
+    loop: true,
+    nav: true,
+    dots: false,
+    navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
+
+$('.home_group .owl-carousel').owlCarousel({
+    loop: true,
+    dots: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
+
+$('.home_clients .owl-carousel').owlCarousel({
+    loop: true,
+    nav: true,
+    dots: false,
+    navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+    responsive: {
+        0: {
+            items: 2
+        },
+        600: {
+            items: 4
+        },
+        1000: {
+            items: 6
+        }
+    }
+})
